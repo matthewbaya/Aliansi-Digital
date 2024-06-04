9a37fe9b712449c53ad6c8ed15b11dcc7c34f36e
Matthew Baya <chiptiep99@gmail.com>
2024-06-04 23:55:49 +0800

    feat: banner component

--- ecommerce-client/src/app/login/page.tsx
+++ ecommerce-client/src/app/login/page.tsx
@@ -1,40 +1,38 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function LoginPage(){
-    return(
-        <>
-        <div className="w-50 container">
+export default function LoginPage() {
+  return (
+    <>
+      <div className="w-50 container">
         <h1 className="display-1 text-center">Hello</h1>
         <form>
- 
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">
-      Email address
-    </label>
-    <input
-      type="email"
-      className="form-control"
-      id="exampleInputEmail1"
-      aria-describedby="emailHelp"
-    />
-    
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">
-      Password
-    </label>
-    <input
-      type="password"
-      className="form-control"
-      id="exampleInputPassword1"
-    />
-  </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
 
-  <Link href='/' type="submit" className="btn btn-primary">
-    Submit
-  </Link>
-</form>
-</div>
-        </>
-    )
+          <Link href="/" type="submit" className="btn btn-primary">
+            Submit
+          </Link>
+        </form>
+      </div>
+    </>
+  );
 }
