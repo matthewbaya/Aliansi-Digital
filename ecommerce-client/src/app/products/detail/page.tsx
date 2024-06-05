d49d028e0758f240317cd18fb9890f01fa1363d9
Matthew Baya <chiptiep99@gmail.com>
2024-06-06 00:26:23 +0800

    feat: product detail page

---
+++ ecommerce-client/src/app/products/detail/page.tsx
@@ -0,0 +1,16 @@
+export default function ProductDetail() {
+  return (
+    <>
+      <div className="container">
+        <img
+          src="https://digitalalliance.co.id/wp-content/uploads/2023/07/luna-x2-white-01-1024x1024.webp"
+          alt=""
+        />
+        <h2>Specification</h2>
+        <a href="" className="btn btn-primary">
+          Add to wishlist
+        </a>
+      </div>
+    </>
+  );
+}
